e32ed6ae25df090baff1d97b3405ee7303768a32
rjp0008 <rjp0008@auburn.edu>
2014-06-02 22:52:02 -0500

    Solution to 40
    Stringbuilder is FAST, way better than just plain appending to a string

---
+++ solutions/31-40/e40.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pecs
+{
+    class e40
+    {
+        static void Main(string[] args)
+          {
+            StringBuilder test = new StringBuilder("");
+            for (int i = 1; i <= 1000000; i++)
+            {
+                test.Append(i.ToString());
+            }
+            int output = int.Parse(test[0].ToString());
+            output *= int.Parse(test[9].ToString());
+            output *= int.Parse(test[99].ToString());
+            output *= int.Parse(test[999].ToString());
+            output *= int.Parse(test[9999].ToString());
+            output *= int.Parse(test[99999].ToString());
+            output *= int.Parse(test[999999].ToString());
+            Console.WriteLine(output);
+            Console.ReadLine();
+
+        }
+    }
+}
