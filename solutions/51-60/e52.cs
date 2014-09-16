13fb849d6bc72fe0c1f10f3f549a4fbec3e4372a
rjp0008 <rjp0008@auburn.edu>
2014-09-15 21:31:26 -0500

    New solution
    52

---
+++ solutions/51-60/e52.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+namespace pecs
+{
+    class e52
+    {
+        public static void Main()
+        {
+            int starting = 1;
+            while (true)
+            {
+                if (HasSameDigits(starting * 2, starting * 3))
+                    if (HasSameDigits(starting * 2, starting * 4))
+                        if (HasSameDigits(starting * 2, starting * 5))
+                            if (HasSameDigits(starting * 2, starting * 6))
+                                break;
+                starting++;
+            }
+        }
+
+        private static bool HasSameDigits(int first,int second)
+        {
+            foreach (char digit in first.ToString().ToCharArray())
+            {
+                if (!second.ToString().ToCharArray().Contains(digit))
+                    return false;
+            }
+            foreach (char digit in second.ToString().ToCharArray())
+            {
+                if (!first.ToString().ToCharArray().Contains(digit))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
