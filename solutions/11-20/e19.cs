64d6bd62a02edde5197de4731410e2689d884a5e
Robert Perez <robertjasonperez@gmail.com>
2015-09-10 18:54:43 -0500

    Solved problem 19

---
+++ solutions/11-20/e19.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace PE
+{
+    class e19
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SundayCounter(new DateTime(1901, 1, 1), new DateTime(2000, 12, 31)));
+        }
+
+        /// <summary>
+        /// Counts the number of Sundays that start the month from first parameter to second parameter.
+        /// </summary>
+        /// <param name="startDate">The date to start the search.</param>
+        /// <param name="finishDate">The date to end the search.</param>
+        /// <returns></returns>
+        static int SundayCounter(DateTime startDate, DateTime finishDate)
+        {
+            int totalSundays = 0;
+            startDate = GetNextSunday(startDate);
+
+            while (startDate < finishDate)
+            {
+                if (startDate.Day == 1)
+                {
+                    totalSundays++;
+                }
+
+                startDate = startDate.AddDays(7);
+            }
+
+            return totalSundays;
+        }
+
+        /// <summary>
+        /// Returns the next possible DateTime that is a Sunday.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        static DateTime GetNextSunday(DateTime input)
+        {
+            if (input.DayOfWeek == DayOfWeek.Sunday)
+            {
+                return input;
+            }
+
+            int numberOfDaysUntilSunday = 7 - (int)input.DayOfWeek;
+            return input.AddDays(numberOfDaysUntilSunday);
+        }
+    }
+}
