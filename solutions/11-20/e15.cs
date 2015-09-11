03ed2a5d5d2a517dd229579cf24befd26c16c31b
Robert Perez <robertjasonperez@gmail.com>
2015-09-11 00:21:09 -0500

    Solved lattice paths #15
    Number of path options are equal to the sum of the paths below and to
    the right. The bottom right is seeded with 1, along with the bottom row
    and right column.

---
+++ solutions/11-20/e15.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PE
+{
+    class e15
+    {
+        /// <summary>
+        /// The number of paths available is the sum of the number of paths to the right and below the position. 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            long size = 21;
+            var pathCount = new long[size, size];
+            for (int x = 0; x < size; x++)
+            {
+                pathCount[x, size-1] = 1;
+                pathCount[size-1, x] = 1;
+            }
+
+            for (long x = size-2; x >= 0; x--)
+            {
+                pathCount[x, x] = pathCount[x + 1, x] + pathCount[x, x + 1];
+                for(long i = x-1; i >= 0; i--)
+                {
+                    pathCount[i, x] = pathCount[i + 1, x] + pathCount[i, x + 1];
+                    pathCount[x,i] = pathCount[i + 1, x] + pathCount[i, x + 1];
+                }
+            }
+            Console.WriteLine(pathCount[0, 0]);
+        }
+    }
+}
