c4c940a77062afa3b4a4c00fd14f1f455c641560
Robert Perez <rjp0008@auburn.edu>
2019-10-31 16:30:41 -0500

    scipy

--- solutions/41-50/44.py
+++ solutions/41-50/44.py
@@ -15,9 +15,26 @@ def pentagonal_number(index):
 def optimize(x):
     return abs(pentagonal_number(x[0])-pentagonal_number(x[1]))
 
-def constraint(x):
-    return is_pentegonal(x[0]+x[1])
+def constraintsum(x):
+    if is_pentegonal(x[0]+x[1]):
+        return 0
+    return 1
 
-def constraint(x):
-    return is_pentegonal(abs(x[0]-x[1]))
+def constraintdiff(x):
+    if is_pentegonal(abs(x[0]-x[1])):
+        if x[0] >0 and x[1]>0:
+            return 0
+    return 1
 
+def constraint_pos(x):
+    if x[1]>0 and x[0]>0:
+        return 0
+    return 1
+
+sum = {'type': 'eq', 'fun': constraintsum}
+diff = {'type': 'eq', 'fun': constraintdiff}
+pos = {'type': 'eq', 'fun': constraint_pos}
+x = [1, 5]
+solution = minimize(optimize, x, method='SLSQP', constraints=[sum, diff])
+
+print(solution)
